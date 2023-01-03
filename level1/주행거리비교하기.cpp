c28853c164d6c6493fb0ccea9c3bcc84b70833b0
maru-jang <zzjune0709@gmail.com>
2023-01-03 13:10:02 +0900

    Update Level 1 [3/3]

---
+++ level1/주행거리비교하기.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int A, B;
+	cin >> A >> B;
+	if (A>B)
+		cout << "A";
+	else if (A<B)
+		cout << "B";
+	else
+		cout << "same";
+	
+	return 0;
+}
