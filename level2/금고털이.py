057a973440cf439dbba9510ab6ca52efafbbbd35
maru-jang <zzjune0709@gmail.com>
2023-01-04 11:58:53 +0900

    Update 금고털이 python

--- level2/금고털이.py
+++ level2/금고털이.py
@@ -1,31 +1,28 @@
-import heapq
+W, N = map(int, input().split()) # W는 배낭 무게, N은 귀금속 종류 개수
 
-W, N = map(int, input().split())
-
-metal = []
+metal_weight_per_price = []
 
 for i in range(N):
-        (M, P) = map(int, input().split())
-        metal.append((-P,M,P))
+    M, P = map(int, input().split()) #M 금속의 무게, P 무게당 가격
+    metal_weight_per_price.append((P,M)) # 0:P 1:M
 
-heapq.heapify(metal)
-#metal.sort(key = lambda x : x[1], reverse=True)
-#metal = sorted(metal, key = lambda x : x[1], reverse=True)
-earn = 0
+metal_weight_per_price = sorted(metal_weight_per_price, key=lambda x:x[0], reverse=True)
+
+price_sum = 0
+left_W = W
 
 for i in range(N):
-        x = heapq.heappop(metal)
-        m = x[1]
-        p = x[2]
+    left_W = left_W - metal_weight_per_price[i][1]
+    if left_W > 0:
+        price = metal_weight_per_price[i][0] * metal_weight_per_price[i][1]
+        price_sum += price
+    elif left_W < 0:
+        price = metal_weight_per_price[i][0] * (left_W + metal_weight_per_price[i][1])
+        price_sum += price
+        break
+    else:
+        price = metal_weight_per_price[i][0] * metal_weight_per_price[i][1]
+        price_sum += price
+        break
 
-        if W >= m:
-                earn += m*p
-                W= W - m
-                
-        elif 0 < W < m:
-                earn += W*p
-                W = 0
-        else:
-                break
-
-print(earn)
+print(price_sum)
